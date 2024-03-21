7e70688cb27018405d0991fae3320a4dc696f83e
bilguun082 <bilguune060829@gmail.com>
2024-03-21 17:28:08 +0800

    feat: dasgaliig nemsen

--- components/SlideItem.tsx
+++ components/SlideItem.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import { useRouter } from 'expo-router';
+import React from 'react';
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 
 const { width, height } = Dimensions.get('screen');
@@ -15,7 +15,18 @@ interface SlideItemProps {
 
 const SlideItem: React.FC<SlideItemProps> = ({ item }) => {
   const router = useRouter();
-  console.log(item.isLast);
+  if (!item.isLast) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{item.title}</Text>
+        <View style={styles.content}>
+          <Text style={styles.description}>{item.description}</Text>
+          <Text style={styles.description}>{item.price}</Text>
+        </View>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{item.title}</Text>
@@ -27,7 +38,30 @@ const SlideItem: React.FC<SlideItemProps> = ({ item }) => {
           onPress={() => {
             router.push('/Task?param=1');
           }}>
-          <Text>Test</Text>
+          <View
+            style={{
+              backgroundColor: '#5E5DF0',
+              borderRadius: 999,
+              shadowColor: '#5E5DF0',
+              shadowOffset: { width: 0, height: 10 },
+              shadowOpacity: 0.5,
+              shadowRadius: 20,
+              opacity: 1,
+              paddingTop: 8,
+              paddingRight: 18,
+              paddingBottom: 8,
+              paddingLeft: 18,
+            }}>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: '700', // Change to string value
+                lineHeight: 24,
+                color: 'white',
+              }}>
+              Test
+            </Text>
+          </View>
         </TouchableOpacity>
       </View>
     </View>
